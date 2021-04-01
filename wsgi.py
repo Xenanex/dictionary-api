c5b94fdb189adea81ae3f9ade7641757c6dcab24
Thibault GIRARD <anexlecool@gmail.com>
2021-04-01 16:23:14 +0200

    Add help on root path

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 dictionnary = Dictionnary()
 dictionnary.load(url="https://raw.githubusercontent.com/dwyl/english-words/master/words.txt")
 
+@app.route('/', methods=['GET'])
+def user_help():
+    return "Give a word to test in the URL"
+
 @app.route('/<string:word>', methods=['GET'])
 def check_word(word):
     if dictionnary.is_present(word):
