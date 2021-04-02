1e75c529d1208a70a9e7b4027bba7c7fc9be370c
Thibault GIRARD <anexlecool@gmail.com>
2021-04-02 18:39:40 +0200

    [WIP]Add possibility of error on autocomplete

--- dictionnary.py
+++ dictionnary.py
@@ -1,9 +1,10 @@
 import requests
 
 class Dictionnary():
+    MAX_ERROR = 0
 
     def __init__(self):
-        self.root_node = Node(False)
+        self.root_node = Node("")
         pass
 
     def __add_word(self, word):
@@ -57,6 +58,51 @@ class Dictionnary():
             result.extend(self.__autocomplete_rec(f"{curr_word}{letter}", letter_node))
 
         return result
+
+    def __get_start_possibilities_with_error(self, start_with):
+        return self.__get_start_possibilities_with_error_rec(start_with, "", self.root_node, 0)
+
+    def __get_start_possibilities_with_error_rec(self, continue_with, curr_word, curr_node, curr_error):
+        if curr_error > MAX_ERROR or curr_node is None:
+            return []
+
+        if continue_with == "":
+            return [curr_word]
+
+        result = []
+
+        # Replace by "" => take the second letter
+        replaced_letter = continue_with[0]
+        replaced_node = curr_node.childs.get(replaced_letter, Node(""))
+
+        second_letter = continue_with[1:2]
+        next_continue_with = continue_with[2:]
+        next_curr_word = f"{curr_word}"
+        next_curr_node = replaced_node.childs.get(continue_with[1:2])
+        next_curr_error = curr_error + 1
+        result.extend(__get_node_possibilities_with_error_rec(next_continue_with, next_curr_word, next_curr_node, next_curr_error))
+
+        #Replace by all possibilities instead of first letter
+        for letter,next_curr_node in curr_node.childs.items():
+            if letter != continue_with[0]:
+                curr_error += 1
+            next_continue_with = continue_with[1:]
+            next_curr_word = f"{curr_word}{letter}"
+            result.extend(__get_node_possibilities_with_error_rec(next_continue_with, next_curr_word, next_curr_node, next_curr_error))
+
+        return result
+
+
+    def autocomplete_with_error(self, start_with):
+        result = []
+
+        # List of start_possibility
+        start_possibilities = self.__get_start_possibilities_with_error(start_with, MAX_ERROR)
+        for start_possibility in start_possibilities:
+            result.extend(autocomplete(self, start_possibility))
+
+        return result
+
 
 
 
